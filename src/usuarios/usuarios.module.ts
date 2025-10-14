2291aebeb0b7ac57c8dc6fd2306c856cc462f37b
Eduardo Damián Cabaña <edc.damian2@gmail.com>
2025-10-13 21:22:04 -0300

    Implementé archivos básicos para la  tabla de favoritos, falta establecer relaciones con Usuario

--- src/usuarios/usuarios.module.ts
+++ src/usuarios/usuarios.module.ts
@@ -3,11 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 import { UsuariosService } from './usuarios.service'
 import { UsuariosController } from './usuarios.controller'
 import { Usuario } from './usuario.entity'
-
+import { Favorito } from './favoritos/favoritos.entity';
+import { FavoritosService } from './favoritos/favoritos.service';
+import { FavoritosController } from './favoritos/favoritos.controller';
 @Module({
-  imports: [TypeOrmModule.forFeature([Usuario])],
-  controllers: [UsuariosController],
-  providers: [UsuariosService],
-  exports: [UsuariosService],
+  imports: [TypeOrmModule.forFeature([Usuario, Favorito])],
+  controllers: [UsuariosController, FavoritosController],
+  providers: [UsuariosService, FavoritosService],
+  exports: [UsuariosService, FavoritosService],
 })
-export class UsuariosModule {}
+export class UsuariosModule { }
