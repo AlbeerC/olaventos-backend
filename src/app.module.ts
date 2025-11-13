9d9046b5e4ca699946132cbb0a082cc381ad7fae
Alberto Caminos <albeerc@gmail.com>
2025-11-13 09:51:08 -0300

    preparando para hacer el deploy del back

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,12 +13,13 @@ import { EventosModule } from './organizadores/evento.module';
       inject: [ConfigService],
       useFactory: (config: ConfigService) => ({
         type: 'mysql',
-        host: 'localhost',
-        port: 3306,
+        host: config.get<string>('DB_HOST'),
+        port: config.get<number>('DB_PORT'),
         username: config.get<string>('DB_USER'),
         password: config.get<string>('DB_PASSWORD'),
         database: config.get<string>('DB_NAME'),
         entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        autoLoadEntities: true,
         synchronize: true, // solo dev
       }),
     }), UsuariosModule, AuthModule, EventosModule
